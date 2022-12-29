fcff361381151f41d1d40ae096b57c5cbeb841f6
Junle Li <lijunle@gmail.com>
2022-12-28 18:41:01 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,19 @@
   function setMetadatas() {
     var name = document.querySelector('.breadcrumb-item:last-child');
     if (name) {
+      var parts = name.textContent.split('_');
+      var date = parts[0];
+      var title = parts[1];
+
+      var count = Number(document.querySelector('.photos-count').firstChild.textContent);
+      var description = date + "，共" + count + "张图片（视频）"
+      
+      var firstImage = document.querySelector('app-gallery-grid-photo img').src;
+
       addProperty('og:type', 'website');
-      addProperty('og:title', name.textContent);
+      addProperty('og:title', title);
+      addProperty('og:description', description);
+      addProperty('og:image', firstImage);
     }
   }
 
